56635cec4b3b4de6f718cc65ac681ebe96f5418f
jporter <javayporter@gmail.com>
2020-03-14 22:58:32 -0400

    added quesiton container to index.html file and added displayQuestion function to script.js file

---
+++ script.js
@@ -0,0 +1,82 @@
+//Define Variables
+var hide = document.getElementById("question-container").style.display = "none";
+var questions = [
+    {
+       questionText: "My first question?",
+       answers: ["A1", "A2", "A3", "A4"],
+       correctIndex: 0
+    },
+    {
+       questionText: "My first question 2?",
+       answers: ["A1", "A2", "A3", "A4"],
+       correctIndex: 3
+    },
+    {
+       questionText: "My first question 3?",
+       answers: ["A1", "A2", "A3", "A4"],
+       correctIndex: 2
+    },
+    {
+       questionText: "My first question 4?",
+       answers: ["A1", "A2", "A3", "A4"],
+       correctIndex: 0
+    },
+];
+
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+
+//Start Quiz
+var startQuiz = document.querySelector("#start");
+
+startQuiz.addEventListener("click", function () {
+    hideInstroductionButton();
+    hideStartButton();
+    displayQuestion();
+    startTimer();
+});
+
+//Hide Start Button
+function hideStartButton() {
+    document.getElementById("start").style.visibility = "hidden";
+};
+
+//Hide Introduction Text
+function hideInstroductionButton() {
+    document.getElementById("instructions").style.visibility = "hidden";
+};
+
+//Start Timer
+function startTimer(){
+    var secs = 75;
+    setInterval(function(){
+    if(secs >= 0)
+        document.getElementById("timer").innerHTML = secs + " seconds";
+        secs--;
+        if(secs == 00)
+        {
+            clearInterval(secs);
+            // alert("Sorry. Time's up!");
+            
+        }
+       
+        }, 1000);
+};
+
+//Display Quiz Question
+function displayQuestion() {
+    document.getElementById("question-container").style.display = "block";
+};
+
+
+
+
+// function resetTimer()
+// {
+    
+// }
+
+// $("#start").click(function(){
+//     startTimer();
+// });
+
